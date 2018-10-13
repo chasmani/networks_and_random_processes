4a792eabcfd072049a6b54bfa36b264d0dff3460
chasmani <pilgrimcharlie2@gmail.com>
2018-10-13 18:25:59 +0100

    Wrote up large parts of Questions 1 and 2

--- assignment_1.py
+++ assignment_1.py
@@ -173,9 +173,7 @@ def plot_y_and_z(mu, sigma, tmax):
 	plt.show()
 
 
-def empirical_results(mu, sigma, tmax):
-
-	realisations = 500
+def empirical_results(mu, sigma, tmax, realisations=500):
 	
 	results = np.zeros((realisations,tmax))
 	
@@ -211,12 +209,22 @@ def plot_empirical_results():
 	plt.savefig('Emprical Average.png')
 
 
+
+	
 	plt.show()
 	
-plot_empirical_results()
+
+def box_plots():
 	
-def box_plots(mu,sigma,tmax):
+
+	mu = 0
+	sigma = 0.2
+	tmax = 100
 	
+
+	plt.rcParams['figure.figsize'] = [10, 6]
+
+
 	results = empirical_results(mu,sigma,tmax)
 	
 	timestep_10_results = results[:,[9]]
@@ -225,15 +233,31 @@ def box_plots(mu,sigma,tmax):
 	
 	ax1.boxplot(timestep_10_results)
 	ax1.set_yscale("log")
+	ax1.set_title("n=10")
+		
+	ax1.set_ylabel(r'$log(Z_n)$')
 
 	ax2.boxplot(results[:,[99]])
-	ax2.set_yscale("log")
-	
-	
-	
-	
-def empirical_pdf_at_timestep(mu,sigma,tmax, timestep):
+	#ax2.boxplot(results[:,[99]], whis=100) # Plot whiskers right up to all outlying points
+	ax2.set_title("n=100")	
 
+	ax2.set_ylabel(r'$log(Z_n)$')
+	ax2.set_yscale("log")
+
+
+	plt.suptitle("Box plots showing the emprical ranges at n=10 and n=100")
+	plt.savefig('box_plots.png')
+
+
+	plt.show()
+
+def empirical_pdf_at_timestep_original(timestep):
+
+
+	mu = 0
+	sigma = 0.2
+	tmax = 100
+	
 	
 	results = empirical_results(mu,sigma,tmax)
 	timestep_results = results[:,[timestep-1]]
@@ -244,19 +268,62 @@ def empirical_pdf_at_timestep(mu,sigma,tmax, timestep):
 	xx = np.linspace(0,10,1000)
 	#plt.plot(xx, kde(xx))
 	
-	# KDE plot 2
-	sns.kdeplot(timestep_results.reshape(500), gridsize=10000 )
-	
+
 	# Theoretical plot 1
 	rv = stats.lognorm([(sigma)*(timestep**0.5)], scale=np.exp(timestep*mu))
 	plt.plot(xx, rv.pdf(xx))
+
+
+	# KDE plot 2
+	sns.kdeplot(timestep_results.reshape(500), gridsize=10000 )
 	
-	#x1,x2,y1,y2 = plt.axis()
-	#plt.axis((x1,x2,y1,y2))
+	x1,x2,y1,y2 = plt.axis()
+	plt.axis((0,10,y1,y2))
 	
-	# Theoretical plot 2
-	#plt.plot(xx, log_normal_pdf(xx, mu, sigma, timestep))
+
+	plt.show()
 	
+def empirical_pdf_at_timestep(timestep):
+
+
+	mu = 0
+	sigma = 0.2
+	tmax = 100
+	
+
+	plt.rcParams['figure.figsize'] = [10, 6]
+	
+	results = empirical_results(mu,sigma,tmax, 5000)
+	timestep_results = results[:,[timestep-1]]
+	plt.hist(timestep_results, density=True)
+	
+	# KDE plot 1
+	kde = stats.gaussian_kde(timestep_results.reshape(5000))
+	xx = np.linspace(0,10,1000)
+	#plt.plot(xx, kde(xx))
+	
+
+	# Theoretical plot 1
+	rv = stats.lognorm([(sigma)*(timestep**0.5)], scale=np.exp(timestep*mu))
+	#plt.plot(xx, rv.pdf(xx), label="Theoretical Distribution")
+
+	# KDE plot 2
+	sns.kdeplot(timestep_results.reshape(5000), gridsize=100000, label="Empirical KDE")
+	
+	if timestep > 10:
+		x1,x2,y1,y2 = plt.axis()
+		plt.axis((-1,1000,y1,y2))
+
+	plt.xlabel(r'$Z_n$')
+	plt.ylabel("Probablity Density")
+	plt.title(r"Emprical KDE and Theoretical PDF of $Z_n$ at n={}. $\mu_x=0, \sigma_x=0.2$".format(timestep))
+	plt.legend()
+
+	#plt.savefig('emprical_pdf_kde_{}.png'.format(timestep))
+
+	plt.show()
+
+
 def log_normal_pdf(z_input, mu, sigma, timestep):
 
 	pdf_sequence = []
@@ -272,39 +339,48 @@ def log_normal_pdf(z_input, mu, sigma, timestep):
 			pdf_sequence.append(answer)
 	return pdf_sequence
 	
-def ergodic_average(mu,sigma,tmax):
+def ergodic_average():
 	
+	mu = 0
+	sigma = 0.2
+	tmax = 100
+
 	result = one_realisation(mu,sigma,tmax)[1]
 	
 	ergodic_totals = np.cumsum(result)
 	n_sequence = np.array(range(tmax))+1
 	ergodic_averages = np.divide(ergodic_totals, n_sequence)
 	
-	plt.figure(4)
 	plt.plot(range(tmax), ergodic_averages)
- 
 
-# Second part constants
-#mu = -0.02
-#sigma = 0.2
-#tmax = 100
+	plt.ylabel(r'$\bar{\mu}_N$')
+	plt.xlabel("N")
+	plt.title(r"Ergodic Average of $Z_n$ for 1 realisation. $\mu_x=0, \sigma_x=0.2$")
+	
+	plt.savefig('ergodic_average.png')	
+
+	
+	plt.show()
 
 		
-def empirical_tail(mu,sigma,tmax,timestep,scale):
+def empirical_tail(timestep):
 	
+	mu = -0.02
+	sigma = 0.2
+	tmax = 100
+
 	results = empirical_results(mu,sigma,tmax)
 	
-	timestep_10_results = results[:,[timestep-1]].reshape(500,)
-	print(timestep_10_results.shape)
+	timestep_results = results[:,[timestep-1]].reshape(500,)
 	
-	data_size=len(timestep_10_results)
+	data_size=len(timestep_results)
 
 	# Set bins edges
-	data_set=sorted(set(timestep_10_results))
+	data_set=sorted(set(timestep_results))
 	bins=np.append(data_set, data_set[-1]+1)
 
 	# Use the histogram function to bin the data
-	counts, bin_edges = np.histogram(timestep_10_results, bins=bins, density=False)
+	counts, bin_edges = np.histogram(timestep_results, bins=bins, density=False)
 
 	counts=counts.astype(float)/data_size
 
@@ -312,15 +388,22 @@ def empirical_tail(mu,sigma,tmax,timestep,scale):
 	cdf = np.cumsum(counts)
 
 	# Plot the cdf
-	plt.plot(bin_edges[0:-1], np.ones(len(cdf))-cdf,linestyle='--', marker="o", color='b')
+	plt.plot(bin_edges[0:-1], np.ones(len(cdf))-cdf,linestyle='--', marker="o", color='b', markersize=1, label="Empirical Tail")
 	plt.ylim((0,1))
-	plt.ylabel("CDF")
+	plt.ylabel("1-CDF")
+	plt.xlabel(r"$Z_n$")
 	plt.yscale("linear")
 	plt.xscale("log")  # Q _ what axis scales are most informative?
 	plt.grid(True)
 
+
 	rv = stats.lognorm([(sigma)*(timestep**0.5)], scale=np.exp(timestep*mu))
-	plt.plot(bin_edges[0:-1], rv.sf(bin_edges[0:-1]))
-	
+	plt.plot(bin_edges[0:-1], rv.sf(bin_edges[0:-1]), color="red", label="Theoretical Survival Function")
+	plt.legend()
+
+	plt.title(r"Empirical Tail of $Z_n$ at n={}. 500 realisations, $\mu_x=-0.02, \sigma_x=0.2$".format(timestep))
+	plt.savefig('empirical_tail_{}.png'.format(timestep))	
+
 	plt.show()
 
+empirical_tail(10)
